f331a31d31467d23f01d893b32ac1b64ecc581d1
lucisword <mohitkumar8475077272@gmail.com>
2026-05-05 02:05:16 +0530

    Ready for the deploment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 // const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 
@@ -9,10 +11,14 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+//--> main/build>>frontend/npm+ backend/npm + frontend/npm build
+// --> main/start>>run the server.js then import path and then production react app loads.
+const __dirname = path.resolve();
 
+
+//routing 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
 // app.get('/api/auth/login', (req,res)=>{
 //     res.send("get login ");
 // })
@@ -23,7 +29,14 @@ app.use("/api/messages", messageRoutes);
 //     res.send("click here to signout");
 // })
 
+//develoyment into production 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (_, req) => {
+        res.sendFile(path.join(__dirname, "../fontend", "dist", "index.html"))
+    })
+}
 
 
 app.listen(PORT, () => console.log("Server is running on port: " + PORT));
